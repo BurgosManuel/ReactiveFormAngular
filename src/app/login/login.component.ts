4bde7c675358ce43b9ffe43a0c121e0ec46f9016
Manuel <trapecioinv@gmail.com>
2022-04-21 12:02:34 -0300

    Logica movida al componente 'Login', vinculado Template con el controlador y labels conectados.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+//Importamos los modulos y servicios para el formulario
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LoginComponent {
+  // Creamos la propiedad que almacena al formulario
+  form: FormGroup;
+  //Inyectamos el servicio FormBuilder para construir nuestro formulario
+  constructor(private formBuilder: FormBuilder) {
+    // Asignamos a la propiedad el 'grupo' que contiene los distintos FormControl, mediante el método 'formBuilder'
+    this.form = this.formBuilder.group({
+      username: ['', []],
+      email: ['', []],
+      password: ['', []],
+    });
   }
-
 }
