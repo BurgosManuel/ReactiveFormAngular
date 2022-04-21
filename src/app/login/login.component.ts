3602b4e365ed1365220cb2cff07af32e270868a1
Manuel <trapecioinv@gmail.com>
2022-04-21 12:06:48 -0300

    Validadores agregados.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //Importamos los modulos y servicios para el formulario
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +13,11 @@ export class LoginComponent {
   //Inyectamos el servicio FormBuilder para construir nuestro formulario
   constructor(private formBuilder: FormBuilder) {
     // Asignamos a la propiedad el 'grupo' que contiene los distintos FormControl, mediante el método 'formBuilder'
+    // Agregamos también las VALIDACIONES a cada formControl.
     this.form = this.formBuilder.group({
-      username: ['', []],
-      email: ['', []],
-      password: ['', []],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(12)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
 }
