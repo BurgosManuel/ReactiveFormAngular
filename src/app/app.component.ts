b662ad9cdf9bcae150b93e45898735ee4b1502c7
Manuel <trapecioinv@gmail.com>
2022-04-21 11:59:07 -0300

    Formulario HTML con Bootstrap realizado. FormGroup declarado en Componente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//Importamos los modulos y servicios para el formulario
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formularios';
+  // Creamos la propiedad que almacena al formulario
+  form: FormGroup;
+  //Inyectamos el servicio FormBuilder para construir nuestro formulario
+  constructor(private formBuilder: FormBuilder) {
+    // Asignamos a la propiedad el 'grupo' que contiene los distintos FormControl, mediante el método 'formBuilder'
+    this.form = this.formBuilder.group({
+        username: ['', []],
+        email: ['', []],
+        password: ['', []],
+    })
+  }
 }
